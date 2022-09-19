97ce99a243cbbc3613f2ecc5388f5842976bc832
0BankA0 <artursmurans64@gmail.com>
2022-09-19 11:59:44 +0300

    modified:   rekins.py

--- rekins.py
+++ rekins.py
@@ -2,9 +2,12 @@ class Rekins():
     def __init__(self,klients,veltijums,izmers,materials):
         self.klients = klients
         self.veltijums = veltijums
-        self.izmers = izmers
-        self.materials = materials
+        self.izmers = izmers.split(",")
+        self.materials = float(materials)
 
+        
+        
+        
         self.aprekins()
 
     def izdrukat(self):
@@ -17,12 +20,17 @@ class Rekins():
     def aprekins(self):
         darba_samaksa = 15
         PVN = 21
-        produkta_cena = (len(self.veltijums)) * 1.2 + (self.izmers[0]/100 * self.izmers[1]/100 * self.izmers[2]/100)/ 3 * self.materials
+        produkta_cena = (len(self.veltijums)) * 1.2 + int(self.izmers[0])/100 * int(self.izmers[1])/100 * int(self.izmers[2])/100/ 3 * int(self.materials)
         PVN_summa = (produkta_cena + darba_samaksa)*PVN/100 
         rekina_summa = (produkta_cena + darba_samaksa) + PVN_summa
         return rekina_summa 
 
+klients = input("ievadi vardu: ")
+veltijums= input("Ievadi veltījumu: ")
+izmers= input("ievadi izmerus (platums,garums,augstums): ")
+materieals= input("ievadi materiala cenu EUR/m2: ")
 
 
-pirmais = Rekins("Anna","Lai izdodas!",[20,20,20],3.5)
+
+pirmais = Rekins(klients,veltijums,izmers,materieals)
 pirmais.izdrukat()
