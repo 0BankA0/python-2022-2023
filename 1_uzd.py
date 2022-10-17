14227929103ec5a6c43649d987ba7928006bdd56
0BankA0 <artursmurans64@gmail.com>
2022-10-17 11:59:43 +0300

    modified:   1_uzd.py

--- 1_uzd.py
+++ 1_uzd.py
@@ -1,5 +1,24 @@
 class Ballite():
-    def __init__(self,dekoracijas,vciemini,ediens,vel_davanas):
-        dzD = Ballite(["Baloni","Virtene","Salvetes"],["Anna","Pēteris","Zeltīte","Valters"],["Kartupeļi","Gurķi","Burkāni","Kūka","Sula"],["Grāmata","Krājkase","Termokrūze"])
-        
-        print(dzD.ediens)
+    def __init__(self,dekoracijas, ciemini, ediens, vel_davanas):
+        self.dekoracijas = dekoracijas
+        self.ciemini = ciemini
+        self.ediens = ediens
+        self.velDavanas = vel_davanas
+
+    def pirkumi(self):
+        print('nopirc dekoracijas:')
+        for a in self.dekoracijas:
+            print(a)
+        print('Nopirc eienus:')
+        for a in self.ediens:
+            print(a)
+
+    def Davanas(self):
+        print('davanu sarkats:')
+        for a in self.velDavanas:
+            print(a)
+
+dzD = Ballite(["Baloni","Virtene","Salvetes"],["Anna","Pēteris","Zeltīte","Valters"],["Kartupeļi","Gurķi","Burkāni","Kūka","Sula"],["Grāmata","Krājkase","Termokrūze"])
+print(dzD.ediens)
+dzD.pirkumi()
+dzD.Davanas()
